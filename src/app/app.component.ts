c24d551c1d2f97cc66e8f542a4eb5963f1f88e4c
aswin-dotcom <aswin42samuel7@gmail.com>
2025-06-27 18:22:51 +0530

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../AuthService';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amplify';
+  constructor(private authService: AuthService) {
+    // You can use the authService here if needed
+
+  }
+  async ngOnInit() {
+    let user = await this.authService.getCurrentUser();
+    console.log('Current User:', user);
+
+  }
+  formFields = {
+    signUp: {
+      name: {
+        order: 1
+      },
+      email: {
+        order: 2
+      },
+      password: {
+        order: 5
+      },
+      confirm_password: {
+        order: 6
+      }
+    },
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Amplify } from 'aws-amplify';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
+
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_yLyNU4fvo',
+      userPoolClientId: '1gb564rlnnerp9k5dh42dbsl06'
+      // userPoolId: 'us-east-1_Tx25rZ4pZ',
+      // userPoolClientId: '6ga2fnaculdgvao3qgf3veeh4p'
+
+    }
+  }
+});
 
 @NgModule({
   declarations: [
@@ -10,7 +26,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAuthenticatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
